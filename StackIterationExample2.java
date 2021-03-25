fbfa3253106968148981a182a4fb3b6c52f749f1
sj19-Y <79995719+sj19-Y@users.noreply.github.com>
2021-03-25 03:40:54 -0700

    Add files via upload

---
+++ StackIterationExample2.java
@@ -0,0 +1,23 @@
+package stack;
+
+import java.util.Stack;
+
+public class StackIterationExample2 {
+
+	public static void main (String[] args)   
+	{   
+	//creating an instance of Stack class  
+	Stack <Integer> stk = new Stack<>();  
+	//pushing elements into stack  
+	stk.push(119);  
+	stk.push(203);  
+	stk.push(988);  
+	System.out.println("Iteration over the stack using forEach() Method:");  
+	//invoking forEach() method for iteration over the stack  
+	 
+	         
+	char[] n = null;
+	System.out.println(n);  
+	}  
+	}  
+	 
---
+++ StackPeekMethodExample.java
@@ -0,0 +1,21 @@
+package stack;
+
+import java.util.Stack;
+
+public class StackPeekMethodExample {
+
+	public static void main(String[] args)   
+	{  
+	Stack<String> stk= new Stack<>();  
+	// pushing elements into Stack  
+	stk.push("Apple");  
+	stk.push("Grapes");  
+	stk.push("Mango");  
+	stk.push("Orange");  
+	System.out.println("Stack: " + stk);  
+	// Access element from the top of the stack  
+	String fruits = stk.peek();  
+	//prints stack  
+	System.out.println("Element at top: " + fruits);  
+	}  
+	} 
